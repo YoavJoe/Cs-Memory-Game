984fda888b7d9d0bf4a740a5f91f57e66ea3b92b
YoavJoe <44302737+YoavJoe@users.noreply.github.com>
2020-05-05 18:59:51 +0300

    code of memory game menu form functions

---
+++ Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YoavMemoryGame
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Form1 game = new Form1();
+            game.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
